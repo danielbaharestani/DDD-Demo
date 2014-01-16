0198f898034f24e6a5ec07da1a67eca7b8d05088
Daniel Baharestani <baharestani@gmail.com>
2014-01-16 12:15:32 +1100

    Saved the project on old location

--- RepositorySample/RepositorySample/Program.cs
+++ RepositorySample/RepositorySample/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
@@ -9,60 +8,6 @@ namespace RepositorySample
     {
         static void Main(string[] args)
         {
-        }
-    }
-
-    class School
-    {
-        private readonly List<ClassRoom> classRooms = new List<ClassRoom>();
-
-        public void AddClassroom(string name)
-        {
-            classRooms.Add(new ClassRoom(this, name));
-        }
-    }
-
-    class ClassRoom
-    {
-        private readonly School school;
-        private readonly string name;
-
-        public ClassRoom(School school, string name)
-        {
-            this.school = school;
-            this.name = name;
-        }
-
-        public string Name
-        {
-            get { return name; }
-        }
-
-        public School School
-        {
-            get { return school; }
-        }
-
-        public IEnumerable<Studnet> Studnets
-        {
-            get; set;
-        }
-    }
-
-
-    class RegistrationService
-    {
-        public void RegisterStudent(ClassRoom classRoom, Studnet studnet)
-        {
-            
-        }
-    }
-
-    class SchoolRepository
-    {
-        public School FindSchool(string name)
-        {
-            
         }
     }
 }
